d6ab4c55ce657cefc6dbe289740e960a1a9276e3
Pritheev-Roshan <64019196+Pritheev-Roshan@users.noreply.github.com>
2020-05-03 05:57:44 -0700

    WhackAMole Submission

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,137 @@
-package sg.edu.np.WhackAMole;
+package com.example.whackamole;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
-
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
+    private Button ButtonLeft;
+    private Button ButtonMiddle;
+    private Button ButtonRight;
+    private static final String TAG = "Whack A Mole";
+    private int score = 0;
+    private TextView scoreText;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "Starting Game!");
+        changeMole();
+
+        ButtonLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ButtonLeft.getText().toString() == "*") {
+                    score++;
+                    changeMole();
+                    scoreText.setText("" +score);
+                } else {
+                    score--;
+                    changeMole();
+                    scoreText.setText("" +score);
+                }
+                Log.v(TAG, "Button 1 pressed");
+            }
+        });
+
+        ButtonMiddle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ButtonMiddle.getText().toString() == "*") {
+                    score++;
+                    changeMole();
+                    scoreText.setText("" +score);
+                } else {
+                    score--;
+                    changeMole();
+                    scoreText.setText("" +score);
+                }
+                Log.v(TAG, "Button 1 pressed");
+            }
+        });
+
+        ButtonRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ButtonRight.getText().toString() == "*") {
+                    score++;
+                    changeMole();
+                    scoreText.setText("" +score);
+                } else {
+                    score--;
+                    changeMole();
+                    scoreText.setText("" +score);
+                }
+                Log.v(TAG, "Button 1 pressed");
+            }
+        });
+    }
+
+    public void changeMole()
+    {
+        Random random = new Random();
+
+        int randomize = random.nextInt(3);
+
+        if (randomize == 0) {
+
+            ButtonLeft.setText("O");
+            ButtonMiddle.setText("O");
+            ButtonRight.setText("*");
+
+        } else if (randomize == 1) {
+
+            ButtonLeft.setText("O");
+            ButtonMiddle.setText("*");
+            ButtonRight.setText("O");
+
+        } else if (randomize == 2) {
+
+            ButtonLeft.setText("*");
+            ButtonMiddle.setText("O");
+            ButtonRight.setText("O");
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "Destroying App");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "Stopping App");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "Pausing App");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "Resuming App");
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG, "Finished Pre-Initialisation!");
-    }
+        ButtonLeft = (Button) findViewById(R.id.ButtonLeft);
+        ButtonMiddle = (Button) findViewById(R.id.ButtonMiddle);
+        ButtonRight = (Button) findViewById(R.id.ButtonRight);
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-        setNewMole();
-        Log.v(TAG, "Starting GUI!");
-    }
-
-
-    public void setNewMole()
-    {
-        Random ran = new Random();
-        int randomLocation = ran.nextInt(3);
+        scoreText = (TextView) findViewById(R.id.textView);
     }
 }
